e4c599a830ac0785bd7ab5a9cc106f4e50aded3e
Joost Kiens <me@joostkiens.com>
2014-01-04 15:59:37 +0100

    Listen to off btn when fading out

--- WakeUpLight.ino
+++ WakeUpLight.ino
@@ -1,9 +1,3 @@
-#include <libraries/Time/Time.h>
-#include <libraries/TimeAlarms/TimeAlarms.h>
-#include "utility/twi.h"
-#include "Wire.h"
-
-
 const int LED = 9;
 const int BTN = 7;
 
@@ -14,58 +8,77 @@ int state = 0;
 int hasChanged = 0;
 int i = 0;
 
-void setup()
-{
+void setup() {
 	pinMode(LED, OUTPUT);
 	pinMode(BTN, INPUT);
 
 	Serial.begin(9600);
 }
 
-void loop()
-{
+
+void loop() {
+	listenForBtnPress();
+
+	if (state == 1) {
+		turnLedOn();
+	}
+	else {
+		turnLedOff();
+	}
+}
+
+// Turn LED immediately
+// off
+void turnLedOff() {
+	analogWrite(LED, 0);
+}
+
+// Turn LED on, slowly or immediately
+void turnLedOn() {
+	if (hasChanged == 1) {
+		fadeLedIn();
+	}
+	else {
+		analogWrite(LED, 255);
+	}
+}
+
+// Slowly Fade LED in,
+// listen for another button press
+// to turn LED off
+void fadeLedIn() {
+	Serial.println("Fade in");
+	for (i = 0; i <= 255; i++) {
+		// Turn of light & exit
+		// loop is button is pressed
+		listenForBtnPress();
+		if (state == 0) {
+			turnLedOff();
+			break;
+		}
+		analogWrite(LED, i);
+		delay(30);
+	}
+}
+
+// Listens for button press.
+// sets global vars:
+// val, oldVal, state & hasChanged
+//
+// TODO: refactor this, fix all the sie effects
+void listenForBtnPress() {
 	val = digitalRead(BTN);
 
 	//Check if the input is HIGH (Button pressed)
-	if ((val == HIGH) && (oldVal == LOW))
-	{
+	if ((val == HIGH) && (oldVal == LOW)) {
 		Serial.println("Button pressed");
 		state = 1 - state;
 		hasChanged = 1;
 		delay(10);
 	}
-	else{
+	else {
 		hasChanged = 0;
 	}
 
 	oldVal = val;
-
-	if (state == 1)
-	{
-		fadeIn();
-	}
-	else
-	{
-		analogWrite(LED, 0); 
-	}
-}
-
-void fadeIn()
-{
-	if (hasChanged == 1)
-	{
-		Serial.println("Fade in");
-		for (i = 0; i <= 255; i++)
-		{
-			analogWrite(LED, i);
-			// this should be asynchronous
-			// so we can turn off the light
-			// even when it is still fading in
-			delay(10);
-		}
-	}
-	else
-	{
-		analogWrite(LED, 255); 
-	}
 }
